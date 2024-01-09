9f8aa038f7ce1d5ffaac9927b19a6178f8810310
Pietro Freire <freiregabriel98@gmail.com>
2024-01-09 11:18:50 +0000

    Initial commit

---
+++ src/main/java/com/picpaychalange/PicpaychalangeApplication.java
@@ -0,0 +1,13 @@
+package com.picpaychalange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaychalangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaychalangeApplication.class, args);
+	}
+
+}
